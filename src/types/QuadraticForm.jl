a29a58c9bb025f8b8b20f77c42af996af9ce42ff
Mattias Fält <mfalt@users.noreply.github.com>
2018-01-17 20:34:44 +0100

    Remove comment #1

--- src/types/QuadraticForm.jl
+++ src/types/QuadraticForm.jl
@@ -12,13 +12,3 @@ end
 
 +(qf1::QuadraticForm, qf2::QuadraticForm) = QuadraticForm(qf1.P+qf2.P, qf1.q+qf2.q, qf1.r+qf2.r)
 ==(qf1::QuadraticForm, qf2::QuadraticForm) = (qf1.P==qf2.P) && (qf1.q==qf2.q) && (qf1.r==qf2.r)
-
-# function find_minimum_value{T}(qf::QuadraticForm{T})
-#     if qf.P[2] == qf.P[3] == qf.P[4] == zero(T)
-#         return -qf.q[1]^2/(4*qf.P[1])+qf.r
-#     elseif qf.P[1] == qf.P[2] == qf.P[3] == zero(T)
-#         return -qf.q[2]^2/(4*qf.P[4])+qf.r
-#     else #Assume P is not zero
-#         return -qf.q'*(qf.P\qf.q)/4 + qf.r
-#     end
-# end
